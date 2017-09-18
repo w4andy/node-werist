a9c6ba73158eacfdc10fd44e41637b30abea5743
Andreas Diem <develop@andreas-diem.at>
2017-09-18 20:06:40 +0200

    test: changed timeout

--- test/domain.test.js
+++ test/domain.test.js
@@ -8,7 +8,7 @@ const werist = require('../');
 
 
 suite('domain', function() {
-  this.timeout(5000);
+  this.timeout(10000);
   suiteSetup((done) => done());
   suiteTeardown((done) => done());
 
--- test/ipaddress.test.js
+++ test/ipaddress.test.js
@@ -8,7 +8,7 @@ const werist = require('../');
 const util = require('util');
 
 suite('ip address', function() {
-  this.timeout(5000);
+  this.timeout(10000);
   suiteSetup((done) => done());
   suiteTeardown((done) => done());
 
